722933f5c3cb3eaf5b682cdbd51f4957be3e2da0
Iryna Vyshnivetska <Iryna Vyshnivetska>
2016-05-30 17:25:04 +0300

    2

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,19 @@
 public class Calculate {
 	public static void main(String[] arg) {
-		System.out.println("Calculate...");
-	}
+		System.out.println("Calculate... ");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int third = Integer.valueOf(arg[2]);
+		int fourth = Integer.valueOf(arg[3]);
+		int summ = first + second;
+		System.out.println ("Summ" + summ);
+			int diff = third - fourth;
+			System.out.println ("Diff3-4=" + diff);
+				int mult = first * third;
+				System.out.println ("Mult1*3=" + mult);	
+					int div = third / fourth;
+					System.out.println ("Div3/4=" + div);
+                                        	
+              }
 
 }
